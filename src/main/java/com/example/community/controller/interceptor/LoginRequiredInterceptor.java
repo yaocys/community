7f63814c2be35510761b0972d96376b7aa26108a
yaocy <yaosunique@gmail.com>
2022-11-17 23:30:45 +0800

    重理 补充登录模块的用户登录检查注解

--- src/main/java/com/example/community/controller/interceptor/LoginRequiredInterceptor.java
+++ src/main/java/com/example/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -20,14 +20,20 @@ public class LoginRequiredInterceptor implements HandlerInterceptor {
     @Autowired
     private HostHolder hostHolder;
 
+    /**
+     * 处理@LoginRequired注解
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
+            // 多判断一下是否是Method对象，精确一下拦截范围
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
+            // 反射检查注解是否存在
             LoginRequired loginRequired = method.getAnnotation(LoginRequired.class);
             if (loginRequired != null && hostHolder.getUser() == null) {
                 response.sendRedirect(request.getContextPath() + "/login");
+                // 为什么还要return一个false？因为重定向后方法并没有结束，而是会继续执行，这里就是为了组织它继续执行
                 return false;
             }
         }
