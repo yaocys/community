3d948e51edf3ef6e7ff3e8028c79bf11c43da6a6
yaocy <yaosunique@gmail.com>
2023-05-15 19:21:54 +0800

    补充原有的“热榜“接口

--- src/main/java/com/example/community/controller/ApiHomeController.java
+++ src/main/java/com/example/community/controller/ApiHomeController.java
@@ -1,9 +1,12 @@
 package com.example.community.controller;
 
 import com.example.community.common.ApiResult;
+import com.example.community.entity.DiscussPost;
+import com.example.community.entity.User;
 import com.example.community.entity.VO.DiscussPostVO;
 import com.example.community.service.DiscussPostService;
 import com.example.community.service.LikeService;
+import com.example.community.service.UserService;
 import com.example.community.util.HostHolder;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.community.util.CommunityConstant.ENTITY_TYPE_POST;
 
@@ -28,7 +34,29 @@ public class ApiHomeController {
     @Resource
     private LikeService likeService;
     @Resource
+    private UserService userService;
+    @Resource
     private HostHolder hostHolder;
+
+    @ApiOperation("热榜，只加载一页10条数据")
+    @GetMapping("/hot")
+    public ApiResult<List<DiscussPostVO>> queryHot(){
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 1, 10, 1);
+
+        List<DiscussPostVO> discussPostVOList = new ArrayList<>();
+        // 根据每个帖子对象去查询查相应的用户信息，并将这两个对象封装成一个map放到list集合中
+
+        if (list != null) {
+            for (DiscussPost post : list) {
+                User user = userService.findUserById(post.getUserId());
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                boolean likeStatus = hostHolder.getUser() != null && likeService.findEntityLikeStatus(hostHolder.getUser().getId(), ENTITY_TYPE_POST, post.getId());
+                DiscussPostVO discussPostVO = new DiscussPostVO(post, user.getUsername(), user.getHeaderUrl(), likeCount,likeStatus);
+                discussPostVOList.add(discussPostVO);
+            }
+        }
+        return ApiResult.success("热帖排行查询成功",discussPostVOList);
+    }
 
     @ApiOperation("分页 主页帖子列表，每次传递页码，实现一页一页加载数据")
     @GetMapping(path = "/index")
